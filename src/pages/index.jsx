5e3894fb4150dcf6ec605bc44aac2518a8145968
hello-ty <ty.gb3150.wwshz@gmail.com>
2022-02-04 01:28:29 +0900

    🔥

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import clsx from "clsx";
-import classes from "/src/styles/Home.module.css";
 import { Main } from "/src/components/Main/";
 import { Header } from "src/components/Header";
 
